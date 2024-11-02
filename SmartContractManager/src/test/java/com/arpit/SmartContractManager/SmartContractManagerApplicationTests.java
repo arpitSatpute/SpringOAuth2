2bb66b621f645d400a633e635084ce995c2ff152
Arpit-Satpute <arpitsatpute3964@gmail.com>
2024-11-02 16:01:13 +0530

    Spring Security Learning

---
+++ SmartContractManager/src/test/java/com/arpit/SmartContractManager/SmartContractManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpit.SmartContractManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContractManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
